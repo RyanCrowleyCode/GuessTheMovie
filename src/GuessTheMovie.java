5d68501fa396bc75ccf40c51dd3e63f17687537d
Ryan Crowley <RyanCrowley@Ryan-Crowley-MacBook.local>
2020-05-20 16:59:04 -0500

    working on the game logic

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,17 +1,25 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class GuessTheMovie {
 
-    public GuessTheMovie() throws FileNotFoundException {
-    }
+    // I need a helper function that takes randomMovie and an array of characters as arguments.
+    // Function will return a String of correct letters and underscores.
 
     public static void main(String[] args) throws FileNotFoundException {
         Movie movie = new Movie();
         String randomMovie = movie.getRandomMovie();
 
+        Scanner scanner = new Scanner(System.in);
+
         int points = 10;
+        boolean hasWon = false;
 
-        System.out.println(randomMovie);
-
+        System.out.println("I'm thinking of a movie. Can you guess the title?\n");
+        while (points > 0 && !hasWon) {
+            //placeholder switch of conditional statements
+            hasWon = true;
+            points--;
+        }
     }
 }
